cf4b8e39133062f3174af2c91a3c2f352e9657e9
Anggi Riyandi <anggi.riyandi@gmail.com>
2018-09-27 13:46:12 +0700

    routing child

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './shared/login/login.component';
 import { RegisterComponent } from './shared/register/register.component';
+import { GadaiModule } from './gadai/gadai.module';
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
+  {path: 'register', component: RegisterComponent},
+  {path: 'gadai', redirectTo: '/gadai', pathMatch: 'full'}
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+    GadaiModule],
   declarations: [],
   exports: [RouterModule]
 })
