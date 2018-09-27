7b7c2650027070ee84635c0c5d77e578ecc453ff
Anggi Riyandi <anggi.riyandi@gmail.com>
2018-09-27 11:28:27 +0700

    simple login logout flow

--- src/app/shared/auth-service.service.ts
+++ src/app/shared/auth-service.service.ts
@@ -4,6 +4,33 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class AuthServiceService {
-
   constructor() { }
+
+  isLogin(): boolean {
+    if (localStorage.getItem('token') != null &&
+    localStorage.getItem('userInfo') != null) {
+     return true;
+    }
+    return false;
+  }
+
+  login(username: string, password: string): boolean {
+    if ( username === 'anggi' && password === '123') {
+
+      localStorage.setItem('token' , 'asdf123');
+      localStorage.setItem('userInfo', JSON.stringify({
+        'username' : 'anggi',
+        'fullname' : 'anggi'
+      }));
+      return true;
+    }
+
+    return false;
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('userInfo');
+  }
+
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,13 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    FormsModule
   ],
   declarations: [NavbarComponent, RegisterComponent, LoginComponent],
   exports: [NavbarComponent, LoginComponent, RegisterComponent]
